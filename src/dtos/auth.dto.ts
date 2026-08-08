c430249da4f84d25e61d972939d92fa6e3862d0a
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-08-09 00:23:11 +0530

    feat : implement auth dtos

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,16 +1,32 @@
 import { z } from 'zod';
+import { usersTable } from '#db/schema.js';
+import { createInsertSchema, createUpdateSchema } from 'drizzle-orm/zod';
 
-export const SignupDto = z.object({
-  name: z.string().min(2, 'Name must be at least 2 characters long').trim(),
-  username: z.string().min(3, 'Username must be at least 3 characters long').trim(),
-  email: z.email('Invalid email address').trim().toLowerCase(),
-  password: z.string().min(6, 'Password must be at least 6 characters long'),
+// create/ register dto
+export const createUserDto = createInsertSchema(usersTable, {
+  name: (schema) => schema.min(2, 'Name must be atleast 2 character long'),
+  email: () => z.email('Invalid email address'),
+  password: (schema) => schema.min(6, 'Password must be atleast 6 character long'),
 });
 
-export const LoginDto = z.object({
-  email: z.email('Invalid email address').trim().toLowerCase(),
-  password: z.string().min(1, 'Password is required'),
+// login dto
+export const loginUserDto = createUserDto.pick({
+  email: true,
+  password: true,
 });
 
-export type SignupDtoType = z.infer<typeof SignupDto>;
-export type LoginDtoType = z.infer<typeof LoginDto>;
+// update dto
+export const updateUserDto = createUpdateSchema(usersTable, {
+  name: (schema) => schema.min(2, 'Name must be atleast 2 character long'),
+  email: () => z.email('Invalid email address'),
+  password: (schema) => schema.min(6, 'Password must be atleast 6 character long'),
+}).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+// TS types
+export type CreateUserInput = z.infer<typeof createUserDto>;
+export type LoginUserInput = z.infer<typeof loginUserDto>;
+export type UpdateUserInput = z.infer<typeof updateUserDto>;
